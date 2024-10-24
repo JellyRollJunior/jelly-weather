0a86cb566cf6984ba6d9ca4c37119ab9851c5e18
Brandon Lin <brandonlin9716@gmail.com>
2024-10-23 23:38:27 -0700

    Add input for dynamic weather data location retrieval

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import './styles.css'
 import { getWeatherData } from "./apiService.js"
 
 getWeatherData('Taipei');
+
+function initSearchBar() {
+    const form = document.querySelector('form');
+    const searchInput = document.querySelector('#location');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const location = searchInput.value;
+        console.log(location);
+        if (location === '') {
+            // THROW ERROR AND GET MAD!
+        } else {
+            getWeatherData(location);
+        }
+    })
+}
+
+initSearchBar();
