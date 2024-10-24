ac15029f59fc3067b5ec50cd4684a07833c127c6
Brandon Lin <brandonlin9716@gmail.com>
2024-10-23 18:02:06 -0700

    Add error handling to API request function

--- src/apiService.js
+++ src/apiService.js
@@ -1,13 +1,20 @@
-export { getWeatherData }
+export { getWeatherData };
 
 const API_KEY = 'JRPNTC8Y8V73YMJ6EY5B684W7';
 const BASE_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/`;
 
 async function makeApiRequest(endpoint) {
-    const response = await fetch(endpoint, {mode: 'cors'});
-    if (response.ok) {
+    try {
+        const response = await fetch(endpoint, { mode: 'cors' });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
         const json = await response.json();
         console.log(json);
+        return json;
+    } catch (error) {
+        console.log('API request failed', error);
+        throw error;
     }
 }
 
